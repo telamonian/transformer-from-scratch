fixes for masking

pysrc/transformer_from_scratch/transformer.py
@@ -14,7 +14,7 @@ def makeMaskPad(t, pad_elem):
 
 def makeMaskCausal(t):
     _, n_samples = t.shape
-    return th.tril(th.ones(n_samples, n_samples)).reshape(1, 1, n_samples, n_samples)
+    return th.tril(th.ones(n_samples, n_samples, dtype=th.bool)).reshape(1, 1, n_samples, n_samples)
 
 class Transformer(nn.Module):
     def __init__(self, d_model, n_heads, n_vocab, n_vocab_tgt=None, d_ff=None, pad_elem=None, tie_embed=False, tie_output=False):
@@ -40,8 +40,8 @@ class Transformer(nn.Module):
 
         self.positional_enc = PositionalEncoder(n_vocab=self.n_vocab, d_model=self.d_model)
 
-        self.encoder = Encoder(d_model=self.d_model, n_heads=self.n_heads, n_layers=6, d_ff=self.d_ff, mask=None)
-        self.decoder = Decoder(d_model=self.d_model, n_heads=self.n_heads, n_layers=6, d_ff=self.d_ff, mask=None, mask_enc=None)
+        self.encoder = Encoder(d_model=self.d_model, n_heads=self.n_heads, n_layers=6, d_ff=self.d_ff)
+        self.decoder = Decoder(d_model=self.d_model, n_heads=self.n_heads, n_layers=6, d_ff=self.d_ff)
 
         self.output = nn.Linear(self.d_model, self.n_vocab_tgt, bias=False)
 
@@ -58,7 +58,7 @@ class Transformer(nn.Module):
             tgt_mask = makeMaskCausal(tgt)
         else:
             src_mask = makeMaskPad(src, pad_elem=self.pad_elem)
-            tgt_mask = makeMaskPad(tgt, pad_elem=self.pad_elem) | makeMaskCausal(tgt)
+            tgt_mask = makeMaskPad(tgt, pad_elem=self.pad_elem) & makeMaskCausal(tgt)
 
         src = self.embedding_enc(src)*self.d_model_sqrt
         tgt = self.embedding_dec(tgt)*self.d_model_sqrt